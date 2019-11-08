829fd9d08d169cb39e422cf3ae58860a7d6d68b3
Del333ted <del333ted@protonmail.com>
2019-11-08 20:13:04 +0300

    mobile responsive fix

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,11 @@ export default {
   components: { Navbar, Snackbar, CookieLaw }
 };
 </script>
+
+<style lang="scss">
+body {
+  position: fixed !important;
+  width: 100%;
+  height: 100%;
+}
+</style>
