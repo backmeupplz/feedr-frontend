6df3700c5704debdc64f352e02070a5dddfab1a8
del333ted <56775051+del333ted@users.noreply.github.com>
2019-11-09 17:42:05 +0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-app(:dark='$store.state.dark' :class='$store.state.dark ? "grey darken-4" : "grey lighten-4"')
+  v-app(:dark='$store.state.dark' style="height:100%" :class='$store.state.dark ? "grey darken-4" : "grey lighten-4"')
     cookie-law(theme='blood-orange'
     :buttonText='$t("cookie.button")'
     :message='$t("cookie.message")')
@@ -21,7 +21,7 @@ export default {
 </script>
 
 <style lang="scss">
-body {
+html, body{
   position: fixed !important;
   width: 100%;
   height: 100%;
